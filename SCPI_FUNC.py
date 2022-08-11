15383d6d4207084c8c4653760def98dde0a2dacc
Benjamin Lloyd <61776613+benjaminlloyd@users.noreply.github.com>
2022-08-11 10:42:02 -0400

    Update SCPI_FUNC.py

--- SCPI_FUNC.py
+++ SCPI_FUNC.py
@@ -8,22 +8,30 @@ class SCPI_FUNC:
         import time  # For sleep, clock, time and perf_counter
         
     def wavlen(args,lmbd):
-        args.inst.write(':SOUR:CHAN:WAV '+str(lambd)+'nm')
+        args.inst.write(':SOUR:CHAN:WAV '+str(lambd)+'nm') #sets laser wavelength
         
     def power(args,poww):
-        args.inst.write(':POW '+str(poww)+'dBM')
+        args.inst.write(':POW '+str(poww)+'dBM') #set laser power in dBM
+        
+     def switch():
+        state=int( args.inst.query(':POW:STAT?')[:-1]) #gets current state of laser
+        if state==0:
+             args.inst.write(':POW:STAT 1') #changes the state of the laser
+        else:
+             args.inst.write(':POW:STAT 0') #changes the state of the laser
+        
     def setup_autosweep(args,start,stop,speed):
         sig=[]
-        args.inst.write(':WAV:SWE:STAR '+str(start)+'nm')
-        args.inst.write(':WAV:SWE:STOP '+str(stop)+'nm')
+        args.inst.write(':WAV:SWE:STAR '+str(start)+'nm') #minimum wavelength
+        args.inst.write(':WAV:SWE:STOP '+str(stop)+'nm') #maximum wavelength
         args.inst.write('WAV:SWE:MOD 1') #1 for one way, 3 for two way (continuous)
-        args.inst.write(':WAV:SWE:SPE '+str(speed))
-        state=int(args.inst.query(':POW:STAT?')[:-1])
+        args.inst.write(':WAV:SWE:SPE '+str(speed)) #speed of scan nm/s
+        state=int(args.inst.query(':POW:STAT?')[:-1]) #turns on laser if off
         if state==0:
             args.inst.write(':POW:STAT 1')
-        args.inst.write(':WAV '+str(start)+'nm')
+        args.inst.write(':WAV '+str(start)+'nm') #sets the current wavelength to that of the sweep start
 
     return 
 
     def trigauto(args):
-        args.inst.write(':WAV:SWE 1')
+        args.inst.write(':WAV:SWE 1') #triggers the sweep
